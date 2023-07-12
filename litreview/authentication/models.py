56bfc0fe43d729f858e6272582309e1cce076c02
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2023-07-12 15:48:49 +0200

    Configuration de la classe User qui hérite de la classe AbstractUser et configuration des champs: email, username, password et is_active

--- litreview/authentication/models.py
+++ litreview/authentication/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(blank=False, max_length=150, unique=True)
+    username = models.CharField(blank=False, max_length=150, unique=True)
+    password = models.CharField(blank=False, max_length=150)
+    is_active = models.BooleanField(default=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    
