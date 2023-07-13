c1bbbc39e69e58be1a8ca922e31552fc51113d6f
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2023-07-13 14:18:39 +0200

    Reconfiguration de la classe LoginForm au niveau des champs

--- litreview/authentication/forms.py
+++ litreview/authentication/forms.py
@@ -2,10 +2,7 @@ from django import forms
 
 
 class LoginForm(forms.Form):
-    email = forms.EmailField(
-        max_length=150, widget=forms.EmailInput(attrs={'placeholder': 'Adresse email'})
-    )
-    password = forms.CharField(
-        widget=forms.PasswordInput(attrs={'placeholder': 'Mot de passe'})
-    )
+    username = forms.CharField(max_length=150, widget=forms.TextInput(attrs={'placeholder': 'Pseudo'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Mot de passe'}))
+
 
