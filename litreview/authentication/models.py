8c8a5d377623a42629a5d74342b48404a5a4ce93
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2023-07-13 14:22:10 +0200

    Reconfiguration de la classe User au niveau des champs

--- litreview/authentication/models.py
+++ litreview/authentication/models.py
@@ -3,11 +3,12 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    email = models.EmailField(blank=False, max_length=150, unique=True)
-    username = models.CharField(blank=False, max_length=150, unique=True)
-    password = models.CharField(blank=False, max_length=150)
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    password = models.CharField(max_length=150)
     is_active = models.BooleanField(default=True)
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
-    
+    def __str__(self):
+        return self.username
+
+
