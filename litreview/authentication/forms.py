ad4bce3ad4da41c7e46f55e4ea90a698a1d00368
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2023-07-14 16:54:40 +0200

    Ajout d'un placeholder pour les champs username, email, password1 et password2

--- litreview/authentication/forms.py
+++ litreview/authentication/forms.py
@@ -4,6 +4,11 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class SignupForm(UserCreationForm):
+    username = forms.CharField(max_length=150, widget=forms.TextInput(attrs={'placeholder': 'Pseudo'}))
+    email = forms.CharField(max_length=150, widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Mot de passe'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Confirmer le mot de passe'}))
+
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
         fields = ('username', 'email', 'password1', 'password2')
