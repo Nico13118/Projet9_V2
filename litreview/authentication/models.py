088e14736c95c5c03568bf3a203cf9f11232c744
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2023-07-18 16:54:20 +0200

    Ajout des messages d'erreur concernant les champs username et email

--- litreview/authentication/models.py
+++ litreview/authentication/models.py
@@ -3,9 +3,10 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    username = models.CharField(max_length=100, unique=True)
-    email = models.EmailField(max_length=100, unique=True)
-    password = models.CharField(max_length=150)
+    username = models.CharField(max_length=100, unique=True, error_messages={
+        'unique': "Ce nom d'utilisateur est déjà utilisé"})
+    email = models.EmailField(max_length=100, unique=True, error_messages={
+        'unique': "Cet email existe déjà dans notre base de données"})
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
