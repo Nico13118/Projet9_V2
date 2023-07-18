fad7079fb5c2bedb333fd2e8a5562238b667e0b7
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2023-07-18 16:48:03 +0200

    Configuration des méthodes clean_letter_password1, clean_number_password1 et clean_password2 en lien avec les classes du fichier validators.py

--- litreview/authentication/forms.py
+++ litreview/authentication/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from .validators import ContainsLetterValidator, ContainsNumberValidator, Password1Password2
 
 
 class SignupForm(UserCreationForm):
@@ -8,6 +9,22 @@ class SignupForm(UserCreationForm):
     email = forms.CharField(max_length=150, widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
     password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Mot de passe'}))
     password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Confirmer le mot de passe'}))
+
+    def clean_letter_password1(self):
+        password1 = self.cleaned_data.get('password1')
+        validator = ContainsLetterValidator()
+        validator.validate(password1)
+
+    def clean_number_password1(self):
+        password1 = self.cleaned_data.get('password1')
+        validator = ContainsNumberValidator()
+        validator.validate(password1)
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        validador1 = Password1Password2()
+        validador1.validate(password1, password2)
 
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
