00b6ee9802729eb3b8eb87687689f21e67f1613c
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2023-07-19 14:45:34 +0200

    Petite modification au niveau des messages d'erreur

--- litreview/authentication/validators.py
+++ litreview/authentication/validators.py
@@ -8,7 +8,7 @@ class ContainsLetterValidator:
                                   code='password_no_letters')
 
     def get_help_text(self):
-        return 'Votre mot de passe doit contenir au moins une lettre majuscule ou minuscule!'
+        return 'Lettre majuscule ou minuscule!'
 
 
 class ContainsNumberValidator:
@@ -17,13 +17,13 @@ class ContainsNumberValidator:
             raise ValidationError('Le mot de passe doit contenir au moins un chiffre', code='password_no_numbers')
 
     def get_help_text(self):
-        return 'Votre mot de passe doit contenir au moins un chiffre !'
+        return 'Un chiffre '
 
 
 class Password1Password2:
     def validate(self, password1, password2, user=None):
-        if not password1 == password2:
-            raise ValidationError('Les mots de passe ne sont pas identiques!', code='password1_if_not_password2')
+        if password1 != password2:
+            raise ValidationError('Les mots de passe ne sont pas identiques !', code='passwords_not_identical')
 
     def get_help_texte(self):
-        return 'Les mots de passe ne sont pas identiques!'
+        return 'Mots de passe '
