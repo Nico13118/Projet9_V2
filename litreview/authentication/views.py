97e09f48c37e7c39ab604f9ce91c2a4de9574a5e
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2023-07-24 17:36:08 +0200

    Ajout de la classe CustomPasswordResetView qui et en lien avec la classe CustomPasswordResetForm, cette classe gère les pages html password_reset_form, password_reset_email, password_reset_done, lorsque l'utilisateur saisie son adresse email et clique sur le bouton, l'utilisateur reçoit un email afin qu'il procède à la saisie ed son nouveau mot de passe. Ajout de la CustomPasswordResetConfirmView qui est en lien avec la classe CustomSetPasswordForm, cette classe gère la page html password_reset_confirm et password_reset_complete

--- litreview/authentication/views.py
+++ litreview/authentication/views.py
@@ -1,8 +1,13 @@
+from django.contrib.auth.tokens import default_token_generator
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from . import forms
 from django.contrib.auth import login, authenticate, logout
 from django.views.generic import View
 from django.conf import settings
+from django.contrib.auth.views import PasswordResetView
+from .forms import CustomPasswordResetForm, CustomSetPasswordForm
+from django.contrib.auth.views import PasswordResetConfirmView
 
 
 def signup_page(request):
@@ -19,6 +24,14 @@ def signup_page(request):
 
 def registration_success(request):
     return render(request, 'success.html')
+
+
+def password_reset_complete(request):
+    return render(request, 'password_reset_complete.html')
+
+
+def password_reset_done(request):
+    return render(request, 'password_reset_done.html')
 
 
 def logout_user(request):
@@ -45,6 +58,21 @@ class LoginPageView(View):
             if user is not None:
                 login(request, user)
                 return redirect('home')
-        message = 'Identifiants invalides!!'
+        message = 'Identifiants ou mot de passe invalides !!'
         return render(request, self.template_name, context={'form': form, 'message': message})
 
+
+class CustomPasswordResetView(PasswordResetView):
+    form_class = CustomPasswordResetForm
+    template_name = 'password_reset_form.html'
+    email_template_name = 'password_reset_email.html'
+    token_generator = default_token_generator
+    success_url = reverse_lazy("password_reset_done")
+    from_email = settings.DEFAULT_FROM_EMAIL
+    subject_template_name = "password_reset_subject.txt"
+
+
+class CustomPasswordResetConfirmView(PasswordResetConfirmView):
+    form_class = CustomSetPasswordForm
+    template_name = 'password_reset_confirm.html'
+    success_url = reverse_lazy('password_reset_complete')
