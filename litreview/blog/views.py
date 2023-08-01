08d614a49799c6a77e4c24b41fb931a0f0f3b602
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2023-08-01 18:15:52 +0200

    Configuration de la vue concernant la page flux avec les champs photos et blogs afin d'afficher la photo et le contenu du ticket. Configuration de la méthode request_review de la page Demander une critique (Créer un ticket) cette méthode permet d'enregistrer toutes les informations saisies dans le ticket ainsi que la photo du livre ou d'une revue. Les 2 pages contiennent une fonction @login_required qui permet l'accès à ces pages aux utilisateurs authentifiés

--- litreview/blog/views.py
+++ litreview/blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . import forms, models
 from django.contrib.auth.decorators import login_required
 
 
@@ -9,4 +10,33 @@ def home(request):
 
 @login_required
 def flow(request):
-    return render(request, 'blog/flow.html')
+    photos = models.Photo.objects.all()
+    blogs = models.Blog.objects.all()
+    return render(request, 'blog/flow.html', context={'photos': photos, 'blogs': blogs})
+
+@login_required
+def request_review(request):
+    blog_form = forms.BlogForm()
+    photo_form = forms.PhotoForm()
+    if request.method == 'POST':
+        blog_form = forms.BlogForm(request.POST)
+        photo_form = forms.PhotoForm(request.POST, request.FILES)
+        if all([blog_form.is_valid(), photo_form.is_valid()]):
+            photo = photo_form.save(commit=False)
+            photo.uploader = request.user
+            photo.save()
+            blog = blog_form.save(commit=False)
+            blog.author = request.user
+            blog.photo = photo
+            blog.save()
+            return redirect('flow')
+    context = {
+        'blog_form': blog_form,
+        'photo_form': photo_form,
+    }
+    return render(request, 'blog/request_review.html', context=context)
+
+
+@login_required
+def create_ticket(request):
+    return render(request, 'blog/create_ticket.html')
