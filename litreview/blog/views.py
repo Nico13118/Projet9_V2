f9ad255f0f479a23b9084bd38b1426bfbe19b05f
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2023-08-04 15:18:36 +0200

    Ajout du champs ticket et reviews et configuration du champs tickets_photos avec utilisation de la fonction zip qui permet d'afficher le ticket et la photo dans la page flux. Configuration de la méthode create_ticket qui permet d'enregistrer le formulaire d'un ticket et la photo. Configuration de la méthode create_review_not_in_response qui permet d'enregistrer le formulaire lorsque l'utilisateur créer une critique qui n'est pas en reponse à un ticket

--- litreview/blog/views.py
+++ litreview/blog/views.py
@@ -11,32 +11,54 @@ def home(request):
 @login_required
 def flow(request):
     photos = models.Photo.objects.all()
-    blogs = models.Blog.objects.all()
-    return render(request, 'blog/flow.html', context={'photos': photos, 'blogs': blogs})
+    tickets = models.Ticket.objects.all()
+    reviews = models.Review.objects.all()
 
-@login_required
-def request_review(request):
-    blog_form = forms.BlogForm()
-    photo_form = forms.PhotoForm()
-    if request.method == 'POST':
-        blog_form = forms.BlogForm(request.POST)
-        photo_form = forms.PhotoForm(request.POST, request.FILES)
-        if all([blog_form.is_valid(), photo_form.is_valid()]):
-            photo = photo_form.save(commit=False)
-            photo.uploader = request.user
-            photo.save()
-            blog = blog_form.save(commit=False)
-            blog.author = request.user
-            blog.photo = photo
-            blog.save()
-            return redirect('flow')
-    context = {
-        'blog_form': blog_form,
-        'photo_form': photo_form,
-    }
-    return render(request, 'blog/request_review.html', context=context)
+    tickets_photos = zip(tickets, photos)
+    return render(request, 'blog/flow.html', context={'tickets_photos': tickets_photos, 'reviews': reviews})
 
 
 @login_required
 def create_ticket(request):
-    return render(request, 'blog/create_ticket.html')
+    ticket_form = forms.TicketForm()
+    photo_form = forms.PhotoForm()
+    if request.method == 'POST':
+        ticket_form = forms.TicketForm(request.POST)
+        photo_form = forms.PhotoForm(request.POST, request.FILES)
+        if all([ticket_form.is_valid(), photo_form.is_valid()]):
+            photo = photo_form.save(commit=False)
+            photo.uploader = request.user
+            photo.save()
+            ticket = ticket_form.save(commit=False)
+            ticket.user = request.user
+            ticket.photo = photo
+            ticket.save()
+            return redirect('flow')
+    context1 = {
+        'ticket_form': ticket_form,
+        'photo_form': photo_form,
+    }
+    return render(request, 'blog/create_ticket.html', context=context1)
+
+
+@login_required
+def create_review_not_in_response(request):
+    review_form = forms.ReviewForm()
+    photo_form = forms.PhotoForm()
+    if request.method == 'POST':
+        review_form = forms.ReviewForm(request.POST)
+        photo_form = forms.PhotoForm(request.POST, request.FILES)
+        if all([review_form.is_valid(), photo_form.is_valid()]):
+            photo = photo_form.save(commit=False)
+            photo.uploader = request.user
+            photo.save()
+            review = review_form.save(commit=False)
+            review.user = request.user
+            review.photo = photo
+            review.save()
+            return redirect('flow')
+    context2 = {
+        'review_form': review_form,
+        'photo_form': photo_form,
+    }
+    return render(request, 'blog/create_review_not_in_response.html', context=context2)
