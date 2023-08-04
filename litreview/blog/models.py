a4c798accc05f038ce85bd399f7ee0ece44fcb67
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2023-08-04 15:06:37 +0200

    Ajout d'un champs objects dans la classe Photo afin de pouvoir récupérer tous les objets de cette classe. Création et configuration de la classe Ticket afi que l'utilisateur puisse Demander une critique (Créer un ticket). Création et configuration de la classe Review afin que l'utilisateur puisse créer une critique qui n'est pas en réponse à un ticket. Création et configuration de la classe ReviewResponse afin que l'utilisateur puisse créer une critique en réponse à un ticket

--- litreview/blog/models.py
+++ litreview/blog/models.py
@@ -1,18 +1,44 @@
 from django.conf import settings
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Photo(models.Model):
+    objects = models.Manager()
     image = models.ImageField()
     uploader = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
 
 
-class Blog(models.Model):
+# Créer un ticket (Demander une critique)
+class Ticket(models.Model):
+    objects = models.Manager()
     photo = models.ForeignKey(Photo, null=True, on_delete=models.SET_NULL, blank=True)
     title = models.CharField(max_length=128)
-    content = models.CharField(max_length=5000)
+    description = models.TextField(max_length=8192, blank=True)
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
-    starred = models.BooleanField(default=False)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+
+# Créer une critique (Pas en réponse à un ticket)
+class Review(models.Model):
+    objects = models.Manager()
+    ticket = models.ForeignKey(to=Ticket, on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(5)])
+    headline = models.CharField(max_length=128)
+    body = models.TextField(max_length=8192, blank=True)
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+
+# Créer une critique (en réponse à un ticket)
+class ReviewResponse(models.Model):
+    objects = models.Manager()
+    rating = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(5)])
+    headline = models.CharField(max_length=128)
+    body = models.TextField(max_length=8192, blank=True)
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
 
