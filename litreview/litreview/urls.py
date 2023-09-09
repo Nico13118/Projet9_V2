4b9f3d4ddb2fd01588958ebba3a12b648d1c5a5e
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2023-09-09 12:09:12 +0200

    Configuration du chemin d'accès de la page html edit_review_in_response.html

--- litreview/litreview/urls.py
+++ litreview/litreview/urls.py
@@ -27,10 +27,14 @@ urlpatterns = [
     path('logout/', authentication.views.logout_user, name='logout'),
     path('home/', blog.views.home, name='home'),
     path('flow/', blog.views.flow, name='flow'),
+
     path('review_in_response/<int:ticket_id>', blog.views.review_in_response, name='review_in_response'),
     path('review_not_in_response/', blog.views.review_not_in_response,
          name='review_not_in_response'),
+
+    path('edit_review_in_response/<int:review_id>', blog.views.edit_review_in_response, name='edit_review_in_response'),
     path('posts/', blog.views.posts, name='posts'),
+
     path('subscriptions/', blog.views.follow_users, name='subscriptions'),
 
     path('create_ticket/', blog.views.create_ticket, name='create_ticket'),
