24718b74c6251b474458798cefbea194cd50d828
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2023-09-28 11:49:53 +0200

    Suppression des méthodes get help car elles ne sont pas utilisés

--- litreview/authentication/validators.py
+++ litreview/authentication/validators.py
@@ -7,17 +7,11 @@ class ContainsLetterValidator:
             raise ValidationError('Votre mot de passe doit contenir au moins une lettre majuscule ou minuscule!',
                                   code='password_no_letters')
 
-    def get_help_text(self):
-        return 'Lettre majuscule ou minuscule!'
-
 
 class ContainsNumberValidator:
     def validate(self, password1, user=None):
         if not any(char.isdigit() for char in password1):
             raise ValidationError('Le mot de passe doit contenir au moins un chiffre', code='password_no_numbers')
-
-    def get_help_text(self):
-        return 'Un chiffre '
 
 
 class Password1Password2:
@@ -25,5 +19,3 @@ class Password1Password2:
         if password1 != password2:
             raise ValidationError('Les mots de passe ne sont pas identiques !', code='passwords_not_identical')
 
-    def get_help_text(self):
-        return 'Mots de passe '
