1e9293c0a27986a490d4f951791d28e77052115c
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2023-10-03 09:47:58 +0200

    Supression du paramétrage models.CASCADE du champ image car cela ne convient pas

--- litreview/blog/models.py
+++ litreview/blog/models.py
@@ -5,7 +5,7 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Photo(models.Model):
     objects = models.Manager()
-    image = models.ImageField(on_delete=models.CASCADE)
+    image = models.ImageField()
     uploader = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
 
